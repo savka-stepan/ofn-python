601eda2b82eeefe70c531ca429f79610501c58ce
Stepan <savka.stepan.92@gmail.com>
2021-01-28 18:15:30 +0200

    Add main function for xml order

--- ofn_python/lib/xml_order.py
+++ ofn_python/lib/xml_order.py
@@ -1,6 +1,9 @@
 import datetime as dt
+import ftplib
 import os
+import re
 import requests
+import xml.etree.ElementTree as ET
 
 
 class XMLOrderTemplate:
@@ -8,7 +11,7 @@ class XMLOrderTemplate:
     def __init__(self):
         pass
 
-    def _get_xml_header(self, order, correction):
+    def _get_order_header(self, order, correction):
         return f"""<?xml version="1.0" encoding="UTF-8"?>
 <ORDER type="standard" version="2.1" xmlns="http://www.opentrans.org/XMLSchema/2.1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opentrans.org/XMLSchema/2.1 opentrans_2_1.xsd" xmlns:bmecat="http://www.bmecat.org/bmecat/2005" xmlns:xmime="http://www.w3.org/2005/05/xmlmime" xmlns:xsig="http://www.w3.org/2000/09/xmldsig#">
 <ORDER_HEADER>
@@ -91,6 +94,48 @@ class XMLOrderTemplate:
 </ORDER_HEADER>
 <ORDER_ITEM_LIST>"""
 
+    def _get_order_item(self, item, count, order, correction):
+        return f"""
+<ORDER_ITEM>
+<LINE_ITEM_ID>{count}</LINE_ITEM_ID>
+<PRODUCT_ID>
+    <bmecat:SUPPLIER_PID type="supplier_specific">{item['variant']['sku']}</bmecat:SUPPLIER_PID>
+    <bmecat:SUPPLIER_IDREF type="supplier_specific">{order['distributor']['id']}</bmecat:SUPPLIER_IDREF>
+    <bmecat:INTERNATIONAL_PID type="ean">{correction['ean']}</bmecat:INTERNATIONAL_PID>
+    <bmecat:BUYER_PID type="buyer_specific">{order['user_id']}</bmecat:BUYER_PID>
+    <bmecat:DESCRIPTION_SHORT lang="deu">{correction['description_short']}</bmecat:DESCRIPTION_SHORT>
+    <MANUFACTURER_INFO>
+        <bmecat:MANUFACTURER_IDREF type="manufacturer_specific">{correction['manufacturer_idref']}</bmecat:MANUFACTURER_IDREF>
+        <bmecat:MANUFACTURER_PID>{correction['manufacturer_pid']}</bmecat:MANUFACTURER_PID>
+        <bmecat:MANUFACTURER_TYPE_DESCR lang="eng"></bmecat:MANUFACTURER_TYPE_DESCR>
+    </MANUFACTURER_INFO>
+    </PRODUCT_ID>
+<QUANTITY>{item['quantity']}</QUANTITY>
+<bmecat:ORDER_UNIT></bmecat:ORDER_UNIT>
+<PRODUCT_PRICE_FIX>
+    <bmecat:PRICE_AMOUNT>{item['price']}</bmecat:PRICE_AMOUNT>
+    <TAX_DETAILS_FIX>
+        <bmecat:CALCULATION_SEQUENCE>1</bmecat:CALCULATION_SEQUENCE>
+        <bmecat:TAX_CATEGORY>{correction['tax_category']}</bmecat:TAX_CATEGORY>
+        <bmecat:TAX_TYPE>vat</bmecat:TAX_TYPE>
+        <bmecat:TAX>0.0</bmecat:TAX>
+        <TAX_AMOUNT>0.0</TAX_AMOUNT>
+        <TAX_BASE>0.0</TAX_BASE>
+    </TAX_DETAILS_FIX>
+</PRODUCT_PRICE_FIX>
+<PRICE_LINE_AMOUNT>{float(item['price']) * float(item['quantity'])}</PRICE_LINE_AMOUNT>
+<REMARKS type="customRemark" lang="deu"></REMARKS>
+</ORDER_ITEM>"""
+
+    def _get_order_summary(self, order):
+        return f"""
+</ORDER_ITEM_LIST>
+<ORDER_SUMMARY>
+<TOTAL_ITEM_NUM>{len(order['line_items'])}</TOTAL_ITEM_NUM>
+<TOTAL_AMOUNT>{order['total']}</TOTAL_AMOUNT>
+</ORDER_SUMMARY>
+</ORDER>"""
+
 
 class XMLOrder(XMLOrderTemplate):
 
@@ -100,13 +145,13 @@ class XMLOrder(XMLOrderTemplate):
         self.server_name = server_name
 
     def _get_order_data(self, order_number, headers, params):
+        '''Get order details.'''
         url = f'{self.server_name}/api/orders/{order_number}'
         response = requests.get(url, headers=headers, params=params)
-        order = response.json()
+        return response
 
-        return order
-        
-    def _make_data_correction(self, order):
+    def _make_order_header_correction(self, order):
+        '''Make order header correction.'''
         correction = {}
         correction['completed_at'] = dt.datetime.strptime(order['completed_at'], '%B %d, %Y').strftime(
             '%Y-%m-%dT%H:%M:%S')
@@ -135,13 +180,123 @@ class XMLOrder(XMLOrderTemplate):
 
         return correction
 
-    def generate(self):
-        order_number = 'R324454232'
-        headers = {
-            'Accept': 'application/json;charset=UTF-8',
-            'Content-Type': 'application/json'
-        }
-        params = (('token', os.environ['OPEN_FOOD_NETWORK_API_KEY']),)
-        order = self._get_order_data(order_number, headers, params)
-        correction = self._make_data_correction(order)
-        self.xml_str = self._get_xml_header(order, correction)
+    def _get_product_data(self, item, headers, params):
+        '''Get product details.'''
+        url = f"{self.server_name}/api/products/bulk_products?q[name_eq]={item['variant']['product_name']}"
+        response = requests.get(url, headers=headers, params=params)
+        return response
+
+    def _make_order_item_correction(self, item, eans, headers, params):
+        '''Make order item correction.'''
+        correction = {}
+
+        # Get manufacturer from product details
+        response = self._get_product_data(item, headers, params)
+
+        try:
+            product = response.json()['products'][0]
+        except IndexError:
+            product = None
+
+        if product:
+            correction['manufacturer_idref'] = product['producer_id']
+            correction['manufacturer_pid'] = product['variants'][0]['producer_name']
+        else:
+            correction['manufacturer_idref'] = ''
+            correction['manufacturer_pid'] = ''
+
+        # Get EAN and tax category from google sheet
+        found_sku = eans.loc[eans['sku'] == item['variant']['sku']]
+        if not found_sku.empty:
+            correction['tax_category'] = f"{found_sku.iloc[0]['tax_category']}"
+            found_ean = found_sku.iloc[0]['EAN']
+
+            if found_ean == '':
+                correction['ean'] = item['variant']['sku']
+            else:
+                correction['ean'] = found_ean
+        else:
+            correction['tax_category'] = ''
+            correction['ean'] = item['variant']['sku']
+
+        # Fix product display name
+        if item['variant']['product_name'] == item['variant']['name_to_display']:
+            correction['description_short'] = f"{item['variant']['product_name']} {item['variant']['unit_to_display']}"
+        else:
+            correction['description_short'] = f"{item['variant']['product_name']} {item['variant']['name_to_display']} {item['variant']['unit_to_display']}"
+
+        return correction
+
+    def _iterate_items(self, order, eans, headers, params):
+        '''Iterate through products.'''
+        print('Products:')
+        skus_wrong_format = []
+        for count, item in enumerate(order['line_items'], 1):
+            print(item['variant']['sku'])
+            order_item_correction = self._make_order_item_correction(item, eans, headers, params)
+            # Get all skus with wrong format
+            if not re.match(r'\b\w{3}\-\w{3}\-\d{3,}\b', item['variant']['sku']):
+                skus_wrong_format.append({'sku': item['variant']['sku'],
+                    'producer': order_item_correction['manufacturer_pid']})
+            self.xml_str += self._get_order_item(item, count, order, order_item_correction)
+
+        return skus_wrong_format
+
+    def _replace_special_chr(self, ch1, ch2):
+        '''Replace char1 with char2.'''
+        if ch1 in self.xml_str:
+            self.xml_str = self.xml_str.replace(ch1, ch2)
+
+    def _send_email_wrong_sku_format(self, skus_wrong_format):
+        '''Send email with wrong sku format'''
+        email_subject = 'SKU Fehler in Münsterländer Bauernbox'
+        email_body = 'Es gab ein paar Fehler mit:'
+        for sku_wrong_format in skus_wrong_format:
+            email_body += f"<br>SKU {sku_wrong_format['sku']}, von Produzent {sku_wrong_format['producer']}"
+        email_body += '<br>bitte prüfen.'
+        # send_email([os.environ['EMAIL_HENDIRK_OFN']], email_subject, email_body)
+
+    def _send_email_zip_not_in_range(self, order, delivery_zip):
+        '''Send email if zipcode not in certain range.'''
+        email_subject = 'Falsche Postleitzahl in Münsterländer Bauernbox'
+        email_body = f"Bestellnummer {order['number']}, Postleitzahl {delivery_zip}"
+        email_body += '<br>bitte prüfen.'
+        # send_email(os.environ['EMAIL_HENDIRK_OFN']], email_subject, email_body)
+
+    def _send_by_email(self, filename, tree_b_str):
+        '''Send xml file by email.'''
+        print()
+        # send_email([os.environ['EMAIL_OPENTRANSORDERS']], 'Opentransorders', 'Opentransorders xml files:',
+        #     attachment=[filename, tree_b_str, 'application/xml'])
+
+    def _send_to_ftp_server(self, filename, tree_b_str):
+        '''Send xml file to ftp server.'''
+        with ftplib.FTP(os.environ['BAUERNBOX_FTP_SERVER'], os.environ['BAUERNBOX_FTP_USERNAME'],
+                os.environ['BAUERNBOX_FTP_PASSWORD']) as ftp:
+            ftp.storbinary(f'STOR orders/{filename}', io.BytesIO(tree_b_str))
+
+    def _save_to_local_storage(self, tree, filename):
+        '''Save to local storage.'''
+        tree.write(filename, encoding="utf-8", xml_declaration=True, short_empty_elements=False)
+
+    def generate(self, order_number, headers, params, eans, postal_codes):
+        order = self._get_order_data(order_number, headers, params).json()
+        order_header_correction = self._make_order_header_correction(order)
+        self.xml_str = self._get_order_header(order, order_header_correction)
+        skus_wrong_format = self._iterate_items(order, eans, headers, params)
+        self.xml_str += self._get_order_summary(order)
+        self._replace_special_chr('&', '&amp;')
+
+        if skus_wrong_format:
+            self._send_email_wrong_sku_format(skus_wrong_format)
+
+        if order_header_correction['delivery_zip'] not in postal_codes:
+            self._send_email_zip_not_in_range(order, order_header_correction['delivery_zip'])
+
+        filename = f"opentransorder{order['number']}.xml"
+        tree = ET.ElementTree(ET.fromstring(self.xml_str, ET.XMLParser(encoding='utf-8')))
+        root = tree.getroot()
+        tree_b_str = ET.tostring(root, encoding='utf-8', method='xml')
+        # self._send_by_email(filename, tree_b_str)
+        # self._send_to_ftp_server(filename, tree_b_str)
+
--- ofn_python/scripts/generate_xml.py
+++ ofn_python/scripts/generate_xml.py
@@ -1,7 +1,97 @@
 from ofn_python.lib.xml_order import XMLOrder
 
+import datetime as dt
+import os
+import gspread
+import pandas as pd
+import requests
+
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+SCOPES = [
+    'https://spreadsheets.google.com/feeds',
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/spreadsheets'
+]
+CREDENTIALS_FILE = 'creds/openfoodnetwork-9e79b28ba490.json'
+
+
+def get_data_from_google_sheet(filename):
+    creds = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, SCOPES)
+    client = gspread.authorize(creds)
+    sheet = client.open(filename)
+    worksheet_list = sheet.worksheets()
+
+    data = pd.DataFrame()
+    for worksheet in worksheet_list:
+        sheet_df = pd.DataFrame(worksheet.get_all_records())
+        try:
+            sheet_df = sheet_df[['sku', 'tax_category', 'EAN']]
+        except KeyError:
+            sheet_df = pd.DataFrame(columns=['sku', 'tax_category', 'EAN'])
+        data = data.append(sheet_df, ignore_index=True)
+
+    return data
+
 
 def run():
-    xml_order = XMLOrder('https://openfoodnetwork.de')
-    xml_order.generate()
-    print(xml_order.xml_str)
+    today = dt.datetime.today().date()
+    url = f'https://openfoodnetwork.de/api/orders?q[completed_at_gt]={today}&q[state_eq]=complete&q[distributor_name_eq]=Münsterländer Bauernbox eG (iG) '
+    headers = {
+        'Accept': 'application/json;charset=UTF-8',
+        'Content-Type': 'application/json'
+    }
+    params = (('token', os.environ['OPEN_FOOD_NETWORK_API_KEY']),)
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()
+
+    if data['orders']:
+        orders = pd.json_normalize(data['orders']).sort_values('id').reset_index(drop=True)
+        creds = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, SCOPES)
+        client = gspread.authorize(creds)
+        worksheet = client.open('Bauernbox Übersicht').sheet1
+        all_records = worksheet.get_all_records()
+        sheet_df = pd.DataFrame(all_records)
+        orders = orders[~orders['number'].isin(sheet_df['number'].tolist())].reset_index(drop=True)
+
+        if not orders.empty:
+            orders = orders[['id', 'number', 'user_id', 'full_name', 'email', 'phone',
+            'completed_at', 'display_total', 'edit_path', 'state', 'payment_state',
+            'shipment_state', 'payments_path', 'ready_to_ship', 'created_at',
+            'distributor_name', 'special_instructions', 'display_outstanding_balance', 'item_total',
+            'adjustment_total', 'payment_total', 'total', 'distributor.id', 'order_cycle.id']]
+
+            orders.rename(columns={'distributor.id': 'distributor_id',
+                'order_cycle.id': 'order_cycle_id'}, inplace=True)
+
+            ofn_server_name = 'https://openfoodnetwork.de'
+            eans = get_data_from_google_sheet('Produktliste_MSB_XXX_Artikelstammdaten')
+            postal_codes = ['48143', '48147', '48145', '48157', '48159', '48151', '48155', '48153',
+            '48161', '48167', '48165', '48163', '48149']
+
+            for i in orders.itertuples():
+                print(i.number, i.full_name, i.total, i.completed_at)
+                xml_order = XMLOrder(ofn_server_name)
+                xml_order.generate(i.number, headers, params, eans, postal_codes)
+                orders.at[i.Index, 'xml_generated_at'] = dt.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+                print('---')
+
+            orders['payments_path'] = orders['payments_path'].apply(lambda x: ofn_server_name + x)
+            orders['edit_path'] = orders['edit_path'].apply(lambda x: ofn_server_name + x)
+            orders['created_at'] = orders['created_at'].apply(lambda x: dt.datetime.strptime(x,
+                '%B %d, %Y').strftime('%Y-%m-%d'))
+            orders['completed_at'] = orders['completed_at'].apply(lambda x:
+                dt.datetime.strptime(x, '%B %d, %Y').strftime('%Y-%m-%d'))
+            orders['ready_to_capture'] = ''
+            orders = orders[['id', 'number', 'user_id', 'full_name', 'email', 'phone',
+            'completed_at', 'display_total', 'edit_path', 'state', 'payment_state',
+            'shipment_state', 'payments_path', 'ready_to_ship', 'ready_to_capture',
+            'created_at', 'distributor_name', 'special_instructions',
+            'display_outstanding_balance','item_total', 'adjustment_total', 'payment_total',
+            'total', 'distributor_id', 'order_cycle_id', 'xml_generated_at']]
+
+            # Save new orders to Bauernbox Übersicht google table
+            orders.fillna('', inplace=True)
+            orders_lol = orders.values.tolist()
+            worksheet.append_rows(orders_lol)
